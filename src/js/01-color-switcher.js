a8fafe704289e7b961bdc5f26ab6d4012f42d800
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-10-29 17:16:33 +0200

    ertg

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,85 @@
 
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+  
+});
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+// const startBtn = document.querySelector(".data-start");
+// const stopBtn = document.querySelector(".data-stop");
+// let timerId = null;
+
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     //console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   //console.log(`Interval with id ${timerId} has stopped!`);
+// });
+
+
+
+
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//   }
+
+
+
+
+//   const colorChangeBtn = document.querySelector('.change-color');
+// const spanEl = document.querySelector('.color');
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// colorChangeBtn.addEventListener('click', changeColorButtonHandler)
+
+// function changeColorButtonHandler() {
+//   const color = getRandomHexColor();
+//   document.body.style.backgroundColor = color;
+//   spanEl.textContent = color;
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,65 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const submitBtn = document.querySelector('button[type="submit"]');
+let timerId = null;
+let promisesCount = 1;
+
+formEl.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  submitBtn.disabled = true;
+
+  const elements = e.currentTarget.elements
+  const delay = elements.delay.value
+  const step = elements.step.value
+  const amount = elements.amount.value
+
+  setTimeout(() => {
+    showPromises();
+    timerId = setInterval(() => {
+      showPromises();
+    }, step)
+  }, delay)
+
+
+function showPromises() {
+  const promise = createPromise(promisesCount, delay);
+
+  promisesCount += 1;
+
+  if (promisesCount > Number(amount)) {
+    clearInterval(timerId);
+    promisesCount = 1;
+    submitBtn.disabled = false;
   }
+  promise.then(result => {
+    Notiflix.Notify.success(result);
+  }).catch(error => {
+    Notiflix.Notify.failure(error);
+  });
 }
+
+}
+function createPromise(position, delay) {
+  
+  const shouldResolve = Math.random() > 0.3; 
+
+  const elements = formEl.elements
+  const step = elements.step.value
+
+  let delayInMs = Number(delay) + step * (promisesCount - 1);
+
+  return new Promise((res, rej) => {
+
+  setTimeout(() => {
+  if (shouldResolve) {
+    res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  } else {
+    rej(`❌ Rejected promise ${position} in ${delay}ms`);
+  }
+}, delay);
+})
+}
+
